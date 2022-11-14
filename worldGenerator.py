ef774dea27d0b9b8854cc050b808653aa21c9a9b
ServerDaLite <107884123+ServerDaLite@users.noreply.github.com>
2022-11-14 14:02:48 -0500

    Update worldGenerator.py

--- worldGenerator.py
+++ worldGenerator.py
@@ -8,7 +8,15 @@ class World:
 	def display(self):
 		print("\n".join([" ".join(i) for i in self.world]))
 		
+	def place(self, x, y, symbol="@"):
+	    self.world[len(self.world) - y - 1][x] = symbol
+	
+	def obtain(self, x, y):
+	    return self.world[len(self.world) - y - 1][x]
+		
 if __name__ == "__main__":
 	WIDTH, HEIGHT = 5, 5
 	world = World(WIDTH, HEIGHT)
+	world.place(1, 1, "&")
+	world.place(2, 2, "@")
 	world.display()
